729b0b1f42c4ff89ff22478f0974f23ec15cf273
Ali İhsan Özkaya <alihsanozkaya27@gmail.com>
2026-01-04 21:17:39 +0300

    clear commit express

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express";
 import { startTelegramBot } from "./platforms/telegramBot.js";
 
 startTelegramBot();
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.status(200).send("Bot çalışıyor...");
-});
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Bot başladı ${PORT}`);
-});

12d8ca5288834e975681bf151882b22da5fefca7
Ali İhsan Özkaya <alihsanozkaya27@gmail.com>
2026-01-04 21:08:45 +0300

    dotenv small fix

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import { startTelegramBot } from "./platforms/telegramBot.js";
-
-dotenv.config();
 
 startTelegramBot();
 
